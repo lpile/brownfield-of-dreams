3c2034ed338e3d8d6ae6688f482c57600e089b5a
William Homer <billyhomeriii@gmail.com>
2019-07-06 15:39:49 -0600

    add position validation test on video model

---
+++ spec/models/video_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Video, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:position) }
+  end
+end
