a05254f45e782bef905b38aab942f56fa98e512a
William Homer <billyhomeriii@gmail.com>
2019-07-06 16:15:17 -0600

    add instance method bookmarked_videos model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,25 @@ RSpec.describe User, type: :model do
       expect(admin.admin?).to be_truthy
     end
   end
+
+  describe 'instance methods' do
+    it 'bookmarked_videos' do
+      t1 = create(:tutorial, title: 'How to Tie Your Shoes vol1')
+      v1 = create(:video, title: 'The Bunny Ears Technique vol1', tutorial: t1)
+      t2 = create(:tutorial, title: 'How to Tie Your Shoes vol2')
+      v2 = create(:video, title: 'The Bunny Ears Technique vol2', tutorial: t2)
+      t3 = create(:tutorial, title: 'How to Tie Your Shoes vol3')
+      v3 = create(:video, title: 'The Bunny Ears Technique vol3', tutorial: t3)
+      user = create(:user)
+
+      create(:user_video, user_id: user.id, video_id: v1.id)
+      create(:user_video, user_id: user.id, video_id: v3.id)
+
+      results = user.bookmarked_videos
+      
+      expect(results.count).to eq(2)
+      expect(results[0]).to eq(v1)
+      expect(results[1]).to eq(v3)
+    end
+  end
 end
