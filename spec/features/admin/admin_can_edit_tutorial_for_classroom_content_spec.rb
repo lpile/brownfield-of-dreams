5c86054946ed4a82adc0f75a97e81703b14bb636
lpile <logan.pile@gmail.com>
2019-07-07 15:06:18 -0600

    Add admin can change classroom content on tutorial spec

---
+++ spec/features/admin/admin_can_edit_tutorial_for_classroom_content_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'An Admin can edit a tutorial' do
+  let(:admin) { create(:admin) }
+
+  scenario 'to be classroom content' do
+    VCR.use_cassette('admin/can_change_tutorial_classroom_content') do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      tutorial = create(:tutorial)
+
+      visit admin_dashboard_path
+
+      within(first(".admin-tutorial-card")) do
+        click_on 'Edit'
+      end
+
+      expect(current_path).to eq(edit_admin_tutorial_path(tutorial))
+
+      expect(tutorial.classroom).to eq(false)
+
+      click_on 'Classroom Content'
+
+      expect(tutorial.reload.classroom).to eq(true)
+
+      click_on 'Visitor Content'
+
+      expect(tutorial.reload.classroom).to eq(false)
+    end
+  end
+end
