fe5d94393071f70dc296973af20d586e438eb72f
William Homer <billyhomeriii@gmail.com>
2019-07-10 21:38:47 -0600

    add possible_friend method to following API

--- app/models/github/following_api.rb
+++ app/models/github/following_api.rb
@@ -5,11 +5,17 @@ module Github
   # FollowingApi poro
   class FollowingApi
     attr_reader :login,
-                :html_url
+                :html_url,
+                :possible_friend
 
-    def initialize(attributes = {})
+    def initialize(attributes = {}, possible_friend_user_ids)
       @login = attributes[:login]
       @html_url = attributes[:html_url]
+      @possible_friend = check_friend(attributes[:id], possible_friend_user_ids)
+    end
+
+    def check_friend(id, user_ids)
+      user_ids.include?(id)
     end
   end
 end
